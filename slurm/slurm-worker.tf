4da5a5306a0b07e1276b00ffbf1bbf79e4c3d907
Ilya Kargapolov <i.kargapolov@nebius.com>
2024-09-13 16:36:04 +0200

    Terraform with junit reporing (#4)
    * Added tests reporting
    
    * Fix disks
    
    * cuda images
    
    * Driverless images

--- slurm/slurm-worker.tf
+++ slurm/slurm-worker.tf
@@ -21,7 +21,7 @@ resource "nebius_compute_v1_disk" "worker" {
   block_size_bytes    = 4096
   size_bytes          = 549755813888
   type                = "NETWORK_SSD"
-  source_image_family = "ubuntu22.04-cuda12"
+  source_image_family = { image_family = "ubuntu22.04-cuda12" }
 }
 
 resource "nebius_compute_v1_instance" "worker" {
