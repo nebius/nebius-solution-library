ad4db0cd34c39b5b8b040d946881daca04d47c29
Boris Popov <popov.boris@nebius.com>
2024-10-04 14:16:06 +0200

    fixed project

--- soperator/main.tf
+++ soperator/main.tf
@@ -15,12 +15,12 @@ module "slurm" {
     }
   }
   k8s_cluster_name = "slurm-k8s"
-  iam_token        = "<YOUR-IAM-TOKEN>"
-  iam_project_id   = "project-e00aya62zwg6e7mcd3"
+  iam_token        = ""
+  iam_project_id   = "project-e00pjzzrtk1fs3yavy"
   slurm_login_ssh_root_public_keys = [
-    "ENCRYPTION-METHOD HASH USER",
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC66U0dlfhFgUptHTgrFIpfBvCSQofPP/jxbu8DL7ohtxCtYoA6N0kC8ZiMaUeW+84K3k2cRt53FqMG0vvgEFhuI/mk//BQXU7jbPKHhnEY5sO4QUaWRgIeo2zLRHrcSn5Uitw8309/64Ui0eVZvnMRE57ifOPLWEgHiSTD9nNMfb6vAdSFDj4vBOtVrcJxmBiXBQQ+0DQkbiRqI4UfW5YwQ1QToZ8cQSJrl4eX6oNH77fbid4DnTTHUulQztFUw3tQRR3zPkCVu6jazqHd2q/OL5sNdTwV9KOLJArxLkUDjXmVRZVsEY4oObL9a6m8epxOCufyub3it9UdNU/5ff7jj+1+7XYqoasnxrAC3Kqqg+jW3xx9MGDCuPBxJdagQgVBsVne5tT/WXoYMQ2EQ92JvrFS8B9Iw/xlvq5Wz+XwFHhvrKgOlwGJUKdf8RjJxU6Sx7m/fXzXv8INQAaGIQmZN3aQ4nUi+xL/lWdX/Zle5CE2947DY6UHWXrne9oSDGc= borispopovsr@i113070648",
   ]
-  vpc_subnet_id                    = "vpcsubnet-e00mxbs1qjnexp9psf"
+  vpc_subnet_id                    = "vpcsubnet-e00dgdntmhgkeej1z3"
   filestore_jail_submounts         = []
   slurm_cluster_name               = "my-slurm"
   telemetry_grafana_admin_password = "MyPassword"
