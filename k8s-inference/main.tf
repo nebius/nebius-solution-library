8aa3cd429c7dc2faaf097fc01cf1b123fc8aac32
Boris Popov <popov.boris@nebius.com>
2024-10-10 12:06:28 +0200

    fixed filestore bug

--- k8s-inference/main.tf
+++ k8s-inference/main.tf
@@ -37,7 +37,7 @@ resource "nebius_mk8s_v1_node_group" "cpu-only" {
     filesystems = var.enable_filestore ? [
       {
         attach_mode         = "READ_WRITE"
-        mount_tag           = "filestore-device"
+        mount_tag           = "data"
         existing_filesystem = nebius_compute_v1_filesystem.shared-filesystem[0]
       }
     ] : null
