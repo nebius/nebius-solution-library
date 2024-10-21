3bd7f1b39f85dd564472bc39a8d71a660b6cb507
Ilia Kargapolov <i.kargapolov@nebius.com>
2024-10-22 00:12:53 +0200

    Install kuberay cluster as application

--- modules/kuberay/main.tf
+++ modules/kuberay/main.tf
@@ -1,17 +1,13 @@
-resource "helm_release" "kuberay-operator" {
-  name             = var.kuberay_name
-  repository       = var.kuberay_repository_path
-  chart            = var.kuberay_chart_name
-  namespace        = var.kuberay_namespace
-  create_namespace = true
-  version          = "1.1.0"
-  atomic           = true
-  values = [
-    templatefile("${path.module}/files/ray-values.yaml.tftpl", {
-      cpu_platform     = var.cpu_platform
-      gpu_platform     = var.gpu_platform
-      max_gpu_replicas = var.max_gpu_replicas
-      min_gpu_replicas = var.min_gpu_replicas
-    })
-  ]
+resource "nebius_applications_v1alpha1_k8s_release" "this" {
+  parent_id        = var.parent_id
+  cluster_id       = var.cluster_id
+  application_name = var.name
+  namespace        = var.namespace
+  product_slug     = "nebius/ray-cluster"
+  values = templatefile("${path.module}/files/ray-values.yaml.tftpl", {
+    cpu_platform     = var.cpu_platform
+    gpu_platform     = var.gpu_platform
+    max_gpu_replicas = var.max_gpu_replicas
+    min_gpu_replicas = var.min_gpu_replicas
+  })
 }
