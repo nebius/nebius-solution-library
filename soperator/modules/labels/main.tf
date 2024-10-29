6564481751ad16a24eeb34b3c22668efbc94d33f
Dmitry Starov <dmitry.staroff@gmail.com>
2024-10-29 16:23:57 +0100

    [ADD] Labels for separate nodesets + extra GPU labels

--- soperator/modules/labels/main.tf
+++ soperator/modules/labels/main.tf
@@ -1,14 +1,32 @@
 locals {
   const = {
     domain = {
-      slurm = "slurm.nebius.ai"
-      mk8s  = "mk8s.nebius.ai"
+      slurm  = "slurm.nebius.ai"
+      mk8s   = "mk8s.nebius.ai"
+      nebius = "nebius.com"
+      nvidia = "nvidia.com"
     }
 
     cluster_id = "cluster-id"
     name = {
       cluster = "cluster-name"
-      group   = "group-name"
+
+      nodeset = "nodeset"
+      nodesets = {
+        system     = "system"
+        controller = "controller"
+        worker     = "worker"
+        login      = "login"
+      }
+
+      workload = "workload"
+      workloads = {
+        cpu = "cpu"
+        gpu = "gpu"
+      }
+
+      # TODO: remove
+      group = "group-name"
       node_group = {
         cpu = "cpu"
         gpu = "gpu"
@@ -18,13 +36,35 @@ locals {
   }
 
   label_key = {
+    nebius_gpu = "${local.const.domain.nebius}/${local.const.name.workloads.gpu}"
+    nvidia_gpu = "${local.const.domain.nvidia}/${local.const.name.workloads.gpu}"
+
     k8s_cluster_id     = "${local.const.domain.mk8s}/${local.const.cluster_id}"
     k8s_cluster_name   = "${local.const.domain.mk8s}/${local.const.name.cluster}"
     slurm_cluster_name = "${local.const.domain.slurm}/${local.const.name.cluster}"
-    slurm_group_name   = "${local.const.domain.slurm}/${local.const.name.group}"
+    slurm_nodeset      = "${local.const.domain.slurm}/${local.const.name.nodeset}"
+    slurm_workload     = "${local.const.domain.slurm}/${local.const.name.workload}"
+
+    # TODO: remove
+    slurm_group_name = "${local.const.domain.slurm}/${local.const.name.group}"
   }
 
   label = {
+    nebius_gpu = tomap({ (local.label_key.nebius_gpu) = ("true") })
+
+    nodeset = {
+      system     = tomap({ (local.label_key.slurm_nodeset) = (local.const.name.nodesets.system) })
+      controller = tomap({ (local.label_key.slurm_nodeset) = (local.const.name.nodesets.controller) })
+      worker     = tomap({ (local.label_key.slurm_nodeset) = (local.const.name.nodesets.worker) })
+      login      = tomap({ (local.label_key.slurm_nodeset) = (local.const.name.nodesets.login) })
+    }
+
+    workload = {
+      cpu = tomap({ (local.label_key.slurm_workload) = (local.const.name.workloads.cpu) })
+      gpu = tomap({ (local.label_key.slurm_workload) = (local.const.name.workloads.gpu) })
+    }
+
+    # TODO: remove
     group_name = {
       cpu = tomap({
         (local.label_key.slurm_group_name) = (local.const.name.node_group.cpu)
--- soperator/modules/labels/outputs.tf
+++ soperator/modules/labels/outputs.tf
@@ -1,3 +1,70 @@
+output "name_nodeset_system" {
+  description = "System nodeset name."
+  value       = local.const.name.nodesets.system
+}
+
+output "name_nodeset_controller" {
+  description = "Controller nodeset name."
+  value       = local.const.name.nodesets.controller
+}
+
+output "name_nodeset_worker" {
+  description = "Worker nodeset name."
+  value       = local.const.name.nodesets.worker
+}
+
+output "name_nodeset_login" {
+  description = "Login nodeset name."
+  value       = local.const.name.nodesets.login
+}
+
+output "name_workload_cpu" {
+  description = "CPU workload name."
+  value       = local.const.name.workloads.cpu
+}
+
+output "name_workload_gpu" {
+  description = "GPU workload name."
+  value       = local.const.name.workloads.gpu
+}
+
+output "label_nebius_gpu" {
+  description = "Nebius GPU label."
+  value       = local.label.nebius_gpu
+}
+
+output "label_nodeset_system" {
+  description = "System nodeset label."
+  value       = local.label.nodeset.system
+}
+
+output "label_nodeset_controller" {
+  description = "Controller nodeset label."
+  value       = local.label.nodeset.controller
+}
+
+output "label_nodeset_worker" {
+  description = "Worker nodeset label."
+  value       = local.label.nodeset.worker
+}
+
+output "label_nodeset_login" {
+  description = "Login nodeset label."
+  value       = local.label.nodeset.login
+}
+
+output "label_workload_cpu" {
+  description = "CPU workload label."
+  value       = local.label.workload.cpu
+}
+
+output "label_workload_gpu" {
+  description = "GPU workload label."
+  value       = local.label.workload.gpu
+}
+
+# region TODO: remove
+
 output "name_node_group_cpu" {
   description = "CPU node group name."
   value       = local.const.name.node_group.cpu
@@ -28,6 +95,18 @@ output "label_group_name_nlb" {
   value       = local.label.group_name.nlb
 }
 
+# endregion TODO: remove
+
+output "key_nebius_gpu" {
+  description = "Nebius GPU label key."
+  value       = local.label_key.nebius_gpu
+}
+
+output "key_nvidia_gpu" {
+  description = "Nvidia GPU label key."
+  value       = local.label_key.nvidia_gpu
+}
+
 output "key_k8s_cluster_id" {
   description = "k8s cluster ID label key."
   value       = local.label_key.k8s_cluster_id
@@ -38,6 +117,17 @@ output "key_k8s_cluster_name" {
   value       = local.label_key.k8s_cluster_name
 }
 
+output "key_slurm_nodeset_name" {
+  description = "Slurm nodeset label key."
+  value       = local.label_key.slurm_nodeset
+}
+
+output "key_slurm_workload_name" {
+  description = "Slurm workload label key."
+  value       = local.label_key.slurm_workload
+}
+
+# TODO: remove
 output "key_slurm_node_group_name" {
   description = "Slurm node group label key."
   value       = local.label_key.slurm_group_name
