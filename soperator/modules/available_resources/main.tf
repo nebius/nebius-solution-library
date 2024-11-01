60fcbc0e0a1a81d7e7978d97a1eeb4568be8ba03
Dmitry Starov <dmitry.staroff@gmail.com>
2024-11-01 14:19:14 +0100

    [ADD] Resource preset sufficiency for particular node set

--- soperator/modules/available_resources/main.tf
+++ soperator/modules/available_resources/main.tf
@@ -2,44 +2,109 @@ locals {
   # TODO: Get to know exact amount of allocatable resources
   resources = tomap({
     "cpu-e2" = tomap({
-      # Insufficient resource presets
-      # 2vcpu-8gb
-      # 4vcpu-16gb
+      "2vcpu-8gb" = {
+        cpu_cores              = 2 - 0.5
+        memory_gibibytes       = 8 - 1
+        gpus                   = 0
+        gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = false
+          (module.labels.name_nodeset_controller) = false
+          (module.labels.name_nodeset_worker)     = false
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
+      }
+      "4vcpu-16gb" = {
+        cpu_cores              = 4 - 1
+        memory_gibibytes       = 16 - 1
+        gpus                   = 0
+        gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = false
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
+      }
       "8vcpu-32gb" = {
         cpu_cores              = 8 - 2
         memory_gibibytes       = 32 - 10
         gpus                   = 0
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
       "16vcpu-64gb" = {
         cpu_cores              = 16 - 2
         memory_gibibytes       = 64 - 10
         gpus                   = 0
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
       "32vcpu-128gb" = {
         cpu_cores              = 32 - 2
         memory_gibibytes       = 128 - 10
         gpus                   = 0
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
       "48vcpu-192gb" = {
         cpu_cores              = 48 - 2
         memory_gibibytes       = 192 - 10
         gpus                   = 0
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
       "64vcpu-256gb" = {
         cpu_cores              = 64 - 2
         memory_gibibytes       = 256 - 10
         gpus                   = 0
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
       "80vcpu-320gb" = {
         cpu_cores              = 80 - 2
         memory_gibibytes       = 320 - 10
         gpus                   = 0
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
     })
     "gpu-h100-sxm" = tomap({
@@ -48,45 +113,61 @@ locals {
         memory_gibibytes       = 200 - 15
         gpus                   = 1
         gpu_cluster_compatible = false
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
       "8gpu-128vcpu-1600gb" = {
         cpu_cores              = 128 - 2
         memory_gibibytes       = 1600 - 350
         gpus                   = 8
         gpu_cluster_compatible = true
+        sufficient = {
+          (module.labels.name_nodeset_system)     = true
+          (module.labels.name_nodeset_controller) = true
+          (module.labels.name_nodeset_worker)     = true
+          (module.labels.name_nodeset_login)      = true
+          (module.labels.name_nodeset_accounting) = true
+        }
       }
     })
-    "gpu-l40s-a" = tomap({
-      "1gpu-8vcpu-32gb" = {
-        cpu_cores              = 8 - 2
-        memory_gibibytes       = 32 - 10
-        gpus                   = 1
-        gpu_cluster_compatible = false
-      }
-      "1gpu-16vcpu-64gb" = {
-        cpu_cores              = 16 - 2
-        memory_gibibytes       = 64 - 10
-        gpus                   = 1
-        gpu_cluster_compatible = false
-      }
-      "1gpu-24vcpu-96gb" = {
-        cpu_cores              = 24 - 2
-        memory_gibibytes       = 96 - 10
-        gpus                   = 1
-        gpu_cluster_compatible = false
-      }
-      "1gpu-32vcpu-128gb" = {
-        cpu_cores              = 32 - 2
-        memory_gibibytes       = 128 - 10
-        gpus                   = 1
-        gpu_cluster_compatible = false
-      }
-      "1gpu-40vcpu-160gb" = {
-        cpu_cores              = 40 - 2
-        memory_gibibytes       = 160 - 10
-        gpus                   = 1
-        gpu_cluster_compatible = false
-      }
-    })
+
+    # gpu-l40s-a is not supported
+    # "gpu-l40s-a" = tomap({
+    #   "1gpu-8vcpu-32gb" = {
+    #     cpu_cores              = 8 - 2
+    #     memory_gibibytes       = 32 - 10
+    #     gpus                   = 1
+    #     gpu_cluster_compatible = false
+    #   }
+    #   "1gpu-16vcpu-64gb" = {
+    #     cpu_cores              = 16 - 2
+    #     memory_gibibytes       = 64 - 10
+    #     gpus                   = 1
+    #     gpu_cluster_compatible = false
+    #   }
+    #   "1gpu-24vcpu-96gb" = {
+    #     cpu_cores              = 24 - 2
+    #     memory_gibibytes       = 96 - 10
+    #     gpus                   = 1
+    #     gpu_cluster_compatible = false
+    #   }
+    #   "1gpu-32vcpu-128gb" = {
+    #     cpu_cores              = 32 - 2
+    #     memory_gibibytes       = 128 - 10
+    #     gpus                   = 1
+    #     gpu_cluster_compatible = false
+    #   }
+    #   "1gpu-40vcpu-160gb" = {
+    #     cpu_cores              = 40 - 2
+    #     memory_gibibytes       = 160 - 10
+    #     gpus                   = 1
+    #     gpu_cluster_compatible = false
+    #   }
+    # })
   })
 }
---
+++ soperator/modules/available_resources/terraform.tf
@@ -0,0 +1,3 @@
+module "labels" {
+  source = "../labels"
+}
