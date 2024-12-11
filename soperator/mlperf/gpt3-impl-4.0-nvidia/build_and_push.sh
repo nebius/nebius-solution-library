4100f119ac1107f390755e69dce120b34b884e08
Dmitry Starov <dmitry.staroff@gmail.com>
2024-12-11 16:55:16 +0100

    [ENH] Use new Container Registry for GPT3 image

--- soperator/mlperf/gpt3-impl-4.0-nvidia/build_and_push.sh
+++ soperator/mlperf/gpt3-impl-4.0-nvidia/build_and_push.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-REGISTRY="cr.ai.nebius.cloud/crnbu823dealq64cp1s6"
-REPOSITORY="nvidia-megatron"
-TAG=$(cat ./VERSION)
+REGISTRY='cr.eu-north1.nebius.cloud/slurm-mlperf-training'
+IMAGE_NAME='gpt3-4.0-nvidia'
+TAG="$(cat ./VERSION)"
 
-echo "Build image"
-docker build -f ./Dockerfile -t $REPOSITORY:$TAG --platform linux/amd64 -m 64G .
+echo 'Build image'
+docker build -f ./Dockerfile -t "${IMAGE_NAME}:${TAG}" --platform linux/amd64 -m 64G .
 
-echo "Tag image"
-docker tag $REPOSITORY:$TAG $REGISTRY/$REPOSITORY:$TAG
+echo 'Tag image'
+docker tag "${IMAGE_NAME}:${TAG}" "${REGISTRY}/${IMAGE_NAME}:${TAG}"
 
-echo "Push image"
-docker push $REGISTRY/$REPOSITORY:$TAG
-
+echo 'Push image'
+docker push "${REGISTRY}/${IMAGE_NAME}:${TAG}"
