daa8f64a090606fe069d0d48c8a4a2c82e7b5aa2
Dmitry Starov <dmitry.staroff@gmail.com>
2024-12-20 15:46:51 +0100

    [ENH] Precise allocatable ephemeral storage calculation [MSP-3774]

--- soperator/installations/example/main.tf
+++ soperator/installations/example/main.tf
@@ -206,32 +206,54 @@ module "slurm" {
 
   resources = {
     system = {
-      cpu_cores                   = local.resources.system.cpu_cores
-      memory_gibibytes            = local.resources.system.memory_gibibytes
-      ephemeral_storage_gibibytes = ceil(var.slurm_nodeset_system.boot_disk.size_gibibytes - module.resources.k8s_ephemeral_storage_reserve.gibibytes)
+      cpu_cores        = local.resources.system.cpu_cores
+      memory_gibibytes = local.resources.system.memory_gibibytes
+      ephemeral_storage_gibibytes = floor(
+        module.resources.k8s_ephemeral_storage_coefficient * var.slurm_nodeset_system.boot_disk.size_gibibytes
+        -module.resources.k8s_ephemeral_storage_reserve.gibibytes
+      )
     }
     controller = {
-      cpu_cores                   = local.resources.controller.cpu_cores
-      memory_gibibytes            = local.resources.controller.memory_gibibytes
-      ephemeral_storage_gibibytes = ceil(var.slurm_nodeset_controller.boot_disk.size_gibibytes - module.resources.k8s_ephemeral_storage_reserve.gibibytes)
+      cpu_cores        = local.resources.controller.cpu_cores
+      memory_gibibytes = local.resources.controller.memory_gibibytes
+      ephemeral_storage_gibibytes = floor(
+        module.resources.k8s_ephemeral_storage_coefficient * var.slurm_nodeset_controller.boot_disk.size_gibibytes
+        -module.resources.k8s_ephemeral_storage_reserve.gibibytes
+      )
     }
     worker = [for i, worker in var.slurm_nodeset_workers :
       {
-        cpu_cores                   = local.resources.workers[i].cpu_cores
-        memory_gibibytes            = local.resources.workers[i].memory_gibibytes
-        ephemeral_storage_gibibytes = ceil(worker.boot_disk.size_gibibytes - 2 * module.resources.k8s_ephemeral_storage_reserve.gibibytes)
-        gpus                        = local.resources.workers[i].gpus
+        cpu_cores        = local.resources.workers[i].cpu_cores
+        memory_gibibytes = local.resources.workers[i].memory_gibibytes
+        ephemeral_storage_gibibytes = (
+          module.k8s.gpu_involved
+          ? floor(
+            module.resources.k8s_ephemeral_storage_coefficient * worker.boot_disk.size_gibibytes
+            -2 * module.resources.k8s_ephemeral_storage_reserve.gibibytes
+          )
+          : floor(
+            module.resources.k8s_ephemeral_storage_coefficient * worker.boot_disk.size_gibibytes
+            -module.resources.k8s_ephemeral_storage_reserve.gibibytes
+          )
+        )
+        gpus = local.resources.workers[i].gpus
       }
     ]
     login = {
-      cpu_cores                   = local.resources.login.cpu_cores
-      memory_gibibytes            = local.resources.login.memory_gibibytes
-      ephemeral_storage_gibibytes = ceil(var.slurm_nodeset_login.boot_disk.size_gibibytes - module.resources.k8s_ephemeral_storage_reserve.gibibytes)
+      cpu_cores        = local.resources.login.cpu_cores
+      memory_gibibytes = local.resources.login.memory_gibibytes
+      ephemeral_storage_gibibytes = floor(
+        module.resources.k8s_ephemeral_storage_coefficient * var.slurm_nodeset_login.boot_disk.size_gibibytes
+        -module.resources.k8s_ephemeral_storage_reserve.gibibytes
+      )
     }
     accounting = var.accounting_enabled ? {
-      cpu_cores                   = local.resources.accounting.cpu_cores
-      memory_gibibytes            = local.resources.accounting.memory_gibibytes
-      ephemeral_storage_gibibytes = ceil(var.slurm_nodeset_accounting.boot_disk.size_gibibytes - module.resources.k8s_ephemeral_storage_reserve.gibibytes)
+      cpu_cores        = local.resources.accounting.cpu_cores
+      memory_gibibytes = local.resources.accounting.memory_gibibytes
+      ephemeral_storage_gibibytes = floor(
+        module.resources.k8s_ephemeral_storage_coefficient * var.slurm_nodeset_accounting.boot_disk.size_gibibytes
+        -module.resources.k8s_ephemeral_storage_reserve.gibibytes
+      )
     } : null
   }
 
