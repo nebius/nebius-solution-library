c240c3bdf1415f82b45f88d512440f011d719108
Liran Jdanov <liran.jdanov@nebius.com>
2024-12-27 17:46:32 +0200

    Output variables fix

--- dsvm/output.tf
+++ dsvm/output.tf
@@ -1,3 +1,6 @@
-output "wg_instance_pib" {
-  value = trimsuffix(nebius_compute_v1_instance.dsvm_instance.status.network_interfaces[0].public_ip_address.address, "/32")
+output "DSVM_Login_URL" {
+  value = "http://${trimsuffix(nebius_compute_v1_instance.dsvm_instance.status.network_interfaces[0].public_ip_address.address, "/32")}:8888"
+}
+output "DSVM_Password" {
+  value = nebius_compute_v1_instance.dsvm_instance.id
 }
