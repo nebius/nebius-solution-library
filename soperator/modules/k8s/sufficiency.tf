52234a788a9c674d3e0174e3bbd76228ff6caefc
Dmitry Starov <dmitry.staroff@gmail.com>
2025-01-22 13:03:30 +0100

    [ADD] Check for minimal boot disk size [MSP-3632]

--- soperator/modules/k8s/sufficiency.tf
+++ soperator/modules/k8s/sufficiency.tf
@@ -2,32 +2,41 @@ locals {
   ng_resources = concat(
     [
       {
-        key      = module.labels.name_nodeset_system
-        resource = var.node_group_system.resource
+        key       = module.labels.name_nodeset_system
+        resource  = var.node_group_system.resource
+        boot_disk = var.node_group_system.boot_disk
       },
       {
-        key      = module.labels.name_nodeset_controller
-        resource = var.node_group_controller.resource
+        key       = module.labels.name_nodeset_controller
+        resource  = var.node_group_controller.resource
+        boot_disk = var.node_group_controller.boot_disk
       },
       {
-        key      = module.labels.name_nodeset_login
-        resource = var.node_group_login.resource
+        key       = module.labels.name_nodeset_login
+        resource  = var.node_group_login.resource
+        boot_disk = var.node_group_login.boot_disk
       }
     ],
     [
       for worker in var.node_group_workers :
       {
-        key      = module.labels.name_nodeset_worker
-        resource = worker.resource
+        key       = module.labels.name_nodeset_worker
+        resource  = worker.resource
+        boot_disk = worker.boot_disk
       }
     ],
     var.node_group_accounting.enabled
     ? [{
-      key      = module.labels.name_nodeset_accounting
-      resource = var.node_group_accounting.spec.resource
+      key       = module.labels.name_nodeset_accounting
+      resource  = var.node_group_accounting.spec.resource
+      boot_disk = var.node_group_accounting.spec.boot_disk
     }]
     : []
   )
+}
+
+data "units_data_size" "boot_disk_minimal" {
+  gibibytes = 128
 }
 
 resource "terraform_data" "check_resource_preset_sufficiency" {
@@ -37,6 +46,11 @@ resource "terraform_data" "check_resource_preset_sufficiency" {
     precondition {
       condition     = module.resources.by_platform[each.value.resource.platform][each.value.resource.preset].sufficient[each.value.key]
       error_message = "Insufficient resource preset `${each.value.resource.preset}` for `${each.value.key}` node group."
+    }
+
+    precondition {
+      condition     = each.value.boot_disk.size_gibibytes >= data.units_data_size.boot_disk_minimal.gibibytes
+      error_message = "Insufficient boot disk size `${each.value.boot_disk.size_gibibytes}` for `${each.value.key}` node group. It has to be at least `${data.units_data_size.boot_disk_minimal.gibibytes}` GiB."
     }
   }
 }
