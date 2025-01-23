5f63843448f182c283a44ce6b503b0849260e012
Liran Jdanov <liran.jdanov@nebius.com>
2025-01-23 16:08:48 +0200

    fix automatic test part

--- bastion/test-resource.tf
+++ bastion/test-resource.tf
@@ -1,5 +1,5 @@
 locals {
-  test_wg_host = trimsuffix(nebius_compute_v1_instance.bastion_instance.status.network_interfaces[0].public_ip_address.address, "/32")
+  test_bst_host = trimsuffix(nebius_compute_v1_instance.bastion_instance.status.network_interfaces[0].public_ip_address.address, "/32")
 }
 
 resource "null_resource" "check_bastion_instance" {
@@ -7,7 +7,7 @@ resource "null_resource" "check_bastion_instance" {
 
   connection {
     user = var.ssh_user_name
-    host = local.test_wg_host
+    host = local.test_bst_host
   }
 
   provisioner "remote-exec" {
@@ -16,16 +16,7 @@ resource "null_resource" "check_bastion_instance" {
       "cloud-init status --wait",
       "ip link show wg0",
       "systemctl -q status wg-quick@wg0.service > /dev/null",
+      ".nebius/bin/nebius iam whoami > /dev/null"
     ]
-  }
-}
-
-resource "null_resource" "check_wireguard_web_ui" {
-  depends_on = [null_resource.check_bastion_instance]
-  count      = var.test_mode ? 1 : 0
-
-  provisioner "local-exec" {
-    interpreter = ["bash", "-c"]
-    command     = "sleep 15 && curl ${local.test_wg_host}:5000"
   }
 }
