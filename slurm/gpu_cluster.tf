f694909c5cce6a4bf8a772aad3379ba32c86dbe7
Boris Popov <popov.boris@nebius.com>
2025-02-05 17:15:25 +0100

    deleted slurm-compute module

--- slurm/gpu_cluster.tf
+++
@@ -1,7 +0,0 @@
-
-resource "nebius_compute_v1_gpu_cluster" "gpu-cluster-slurm" {
-  parent_id         = var.parent_id
-  name              = "gpu-cluster-slurm"
-  infiniband_fabric = "fabric-3" # eu-north1
-  # infiniband_fabric = "fabric-5" # eu-west1
-}
