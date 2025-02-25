412ba83489e91b6aada5cecb71c66f97325578e2
Aaron Miller <aaron@nebius.com>
2025-02-24 21:46:03 -0700

    k8s-training: store terraform state in object storage

--- modules/nfs-server/disks.tf
+++ modules/nfs-server/disks.tf
@@ -1,6 +1,6 @@
 resource "nebius_compute_v1_disk" "nfs-boot-disk" {
   parent_id           = var.parent_id
-  name                = "${var.nfs_disk_name_suffix == "" ? "nfs-boot-disk" : format("nfs-boot-disk-%s", var.nfs_disk_name_suffix)}"
+  name                = var.nfs_disk_name_suffix == "" ? "nfs-boot-disk" : format("nfs-boot-disk-%s", var.nfs_disk_name_suffix)
   block_size_bytes    = 4096
   size_bytes          = 64424509440
   type                = "NETWORK_SSD"
@@ -9,7 +9,7 @@ resource "nebius_compute_v1_disk" "nfs-boot-disk" {
 
 resource "nebius_compute_v1_disk" "nfs-storage-disk" {
   parent_id        = var.parent_id
-  name             = "${var.nfs_disk_name_suffix == "" ? "nfs-storage-disk" : format("nfs-storage-disk-%s", var.nfs_disk_name_suffix)}"
+  name             = var.nfs_disk_name_suffix == "" ? "nfs-storage-disk" : format("nfs-storage-disk-%s", var.nfs_disk_name_suffix)
   block_size_bytes = var.disk_block_size
   size_bytes       = var.nfs_size
   type             = var.disk_type
