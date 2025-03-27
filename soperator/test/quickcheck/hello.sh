3f7201be278ace2979de2246c86bd6c154b2d76f
Aaron Miller <aaron@nebius.com>
2025-03-26 21:21:01 -0700

    fix soperator hello.sh quickcheck

--- soperator/test/quickcheck/hello.sh
+++ soperator/test/quickcheck/hello.sh
@@ -3,9 +3,8 @@
 #SBATCH -J hello
 #SBATCH --output=results/hello.out
 #SBATCH --error=results/hello.out
-#SBATCH --cpus-per-task=60
-#SBATCH --mem-per-cpu=8G
-#SBATCH --gpus=4
+#SBATCH --ntasks=2
+#SBATCH --gpus=8
 
 # Print hello
 srun \
@@ -14,8 +13,6 @@ srun \
 # Allocate some resources
 srun \
   --ntasks=2 \
-  --cpus-per-task=60 \
-  --mem-per-cpu=8G \
   --gpus=4 \
   echo "Run nvidia-smi on $(hostname)" \
   && nvidia-smi
