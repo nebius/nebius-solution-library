04599228b87deeec03547d205fcb86c6fb5719d4
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-05-02 13:11:12 +0200

    split create user and ssh checks, and install package check

--- soperator/modules/active_checks/main.tf
+++ soperator/modules/active_checks/main.tf
@@ -1,5 +1,7 @@
-resource "helm_release" "create_nebius_user_and_ssh_check" {
-  name       = "create-nebius-user-and-ssh-check"
+resource "helm_release" "create_nebius_user" {
+  count = var.checks.create_nebius_user ? 1 : 0
+
+  name       = "create-nebius-user-check"
   repository = local.helm.repository.raw
   chart      = local.helm.chart.raw
   version    = local.helm.version.raw
@@ -7,20 +9,19 @@ resource "helm_release" "create_nebius_user_and_ssh_check" {
   create_namespace = true
   namespace        = var.slurm_cluster_namespace
 
-  values = [templatefile("${path.module}/templates/create_nebius_user_and_ssh_check.yaml.tftpl", {
+  values = [templatefile("${path.module}/templates/create_user_check.yaml.tftpl", {
     slurm_cluster_namespace = var.slurm_cluster_namespace
-    slurm_cluster_ip        = var.slurm_cluster_ip
     slurm_cluster_name      = var.slurm_cluster_name
-    num_of_login_nodes      = var.num_of_login_nodes
-    nebius_user_name        = var.nebius_user_name
+    user_name               = var.checks.nebius_username
+    save_ssh_key            = false
   })]
 
   wait = true
 }
 
-resource "terraform_data" "wait_for_create_nebius_user_and_ssh_check" {
+resource "terraform_data" "wait_for_create_user_nebius_check" {
   depends_on = [
-    helm_release.create_nebius_user_and_ssh_check
+    helm_release.create_nebius_user
   ]
 
   provisioner "local-exec" {
@@ -30,10 +31,148 @@ resource "terraform_data" "wait_for_create_nebius_user_and_ssh_check" {
       [
         "kubectl", "wait",
         "--for=jsonpath='{.status.k8sJobsStatus.lastK8sJobStatus}'=Complete",
-        "--timeout", "1m",
+        "--timeout", "2m",
         "--context", var.k8s_cluster_context,
         "-n", var.slurm_cluster_namespace,
-        "activechecks.slurm.nebius.ai/create-nebius-user-and-ssh-check"
+        "activechecks.slurm.nebius.ai/create-user-${var.checks.nebius_username}"
+      ]
+    )
+  }
+}
+
+resource "helm_release" "create_soperatorchecks_user" {
+  count = var.checks.create_soperatorchecks_user ? 1 : 0
+
+  depends_on = [ 
+    terraform_data.wait_for_create_user_nebius_check
+  ]
+
+  name       = "create-soperatorchecks-user-check"
+  repository = local.helm.repository.raw
+  chart      = local.helm.chart.raw
+  version    = local.helm.version.raw
+
+  create_namespace = true
+  namespace        = var.slurm_cluster_namespace
+
+  values = [templatefile("${path.module}/templates/create_user_check.yaml.tftpl", {
+    slurm_cluster_namespace = var.slurm_cluster_namespace
+    slurm_cluster_name      = var.slurm_cluster_name
+    user_name               = var.checks.soperatorchecks_username
+    save_ssh_key            = true
+  })]
+
+  wait = true
+}
+
+resource "terraform_data" "wait_for_create_user_soperatorchecks_check" {
+  depends_on = [
+    helm_release.create_soperatorchecks_user
+  ]
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command = join(
+      " ",
+      [
+        "kubectl", "wait",
+        "--for=jsonpath='{.status.k8sJobsStatus.lastK8sJobStatus}'=Complete",
+        "--timeout", "2m",
+        "--context", var.k8s_cluster_context,
+        "-n", var.slurm_cluster_namespace,
+        "activechecks.slurm.nebius.ai/create-user-${var.checks.soperatorchecks_username}"
+      ]
+    )
+  }
+}
+
+resource "helm_release" "install_package_check" {
+  count = var.checks.install_package_check_enabled ? 1 : 0
+
+  depends_on = [ 
+    terraform_data.wait_for_create_user_soperatorchecks_check 
+  ]
+
+  name       = "install-package-check"
+  repository = local.helm.repository.raw
+  chart      = local.helm.chart.raw
+  version    = local.helm.version.raw
+
+  create_namespace = true
+  namespace        = var.slurm_cluster_namespace
+
+  values = [templatefile("${path.module}/templates/install_package.yaml.tftpl", {
+    slurm_cluster_namespace = var.slurm_cluster_namespace
+    slurm_cluster_name      = var.slurm_cluster_name
+    user_name               = var.checks.soperatorchecks_username
+  })]
+
+  wait = true
+}
+
+resource "terraform_data" "wait_for_install_package_check" {
+  depends_on = [
+    helm_release.install_package_check
+  ]
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command = join(
+      " ",
+      [
+        "kubectl", "wait",
+        "--for=jsonpath='{.status.k8sJobsStatus.lastK8sJobStatus}'=Complete",
+        "--timeout", "2m",
+        "--context", var.k8s_cluster_context,
+        "-n", var.slurm_cluster_namespace,
+        "activechecks.slurm.nebius.ai/install-package"
+      ]
+    )
+  }
+}
+
+
+resource "helm_release" "ssh_check" {
+  count = var.checks.ssh_check_enabled ? 1 : 0
+
+  depends_on = [ 
+    terraform_data.wait_for_create_user_soperatorchecks_check 
+  ]
+
+  name       = "ssh-check"
+  repository = local.helm.repository.raw
+  chart      = local.helm.chart.raw
+  version    = local.helm.version.raw
+
+  create_namespace = true
+  namespace        = var.slurm_cluster_namespace
+
+  values = [templatefile("${path.module}/templates/ssh_check.yaml.tftpl", {
+    slurm_cluster_namespace = var.slurm_cluster_namespace
+    slurm_cluster_name      = var.slurm_cluster_name
+    num_of_login_nodes      = var.num_of_login_nodes
+    user_name               = var.checks.soperatorchecks_username
+  })]
+
+  wait = true
+}
+
+resource "terraform_data" "wait_for_ssh_check" {
+  depends_on = [
+    helm_release.ssh_check
+  ]
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command = join(
+      " ",
+      [
+        "kubectl", "wait",
+        "--for=jsonpath='{.status.k8sJobsStatus.lastK8sJobStatus}'=Complete",
+        "--timeout", "2m",
+        "--context", var.k8s_cluster_context,
+        "-n", var.slurm_cluster_namespace,
+        "activechecks.slurm.nebius.ai/ssh"
       ]
     )
   }
--- soperator/modules/active_checks/variables.tf
+++ soperator/modules/active_checks/variables.tf
@@ -16,20 +16,40 @@ variable "slurm_cluster_namespace" {
   nullable    = false
 }
 
-variable "slurm_cluster_ip" {
-  description = "IP address to connect to the Slurm cluster with."
-  type        = string
-  nullable    = false
-}
-
 variable "num_of_login_nodes" {
   description = "Number of login nodes in the Slurm cluster."
   type        = number
   nullable    = false
 }
 
-variable "nebius_user_name" {
-  description = "Name of the default created user for Nebius operations."
-  type        = string
-  default     = "nebius"
+variable "checks" {
+  description = "Defines what checks should be enabled."
+  type = object({
+    create_nebius_user = bool
+    nebius_username = string
+    create_soperatorchecks_user = bool
+    soperatorchecks_username = string
+
+    ssh_check_enabled = bool
+    install_package_check_enabled = bool
+  })
+  default = {
+    create_nebius_user = true
+    nebius_username = "nebius"
+    create_soperatorchecks_user = true
+    soperatorchecks_username = "soperatorchecks"
+
+    ssh_check_enabled = true
+    install_package_check_enabled = true
+  }
+
+  validation {
+    condition = !var.checks.create_soperatorchecks_user || var.checks.ssh_check_enabled
+    error_message = "SSH check could not be performed without soperatorchecks user creation."
+  }
+
+  validation {
+    condition = !var.checks.create_soperatorchecks_user || var.checks.install_package_check_enabled
+    error_message = "Install package check could not be performed without soperatorchecks user creation."
+  }
 }
