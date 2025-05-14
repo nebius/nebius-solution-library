00ed7623fbf8a874df609886fe836f75d94deb81
Uburro <grigorii.rochev@nebius.com>
2025-05-14 14:09:49 +0200

    fix cluster name

--- soperator/modules/slurm/main.tf
+++ soperator/modules/slurm/main.tf
@@ -105,7 +105,7 @@ resource "helm_release" "soperator_fluxcd_cm" {
     vmlogs_version                     = var.vmlogs_version
 
 
-    cluster_name        = var.cluster_name
+    cluster_name        = var.name
     public_o11y_enabled = var.public_o11y_enabled
     metrics_collector   = local.metrics_collector
     create_pvcs         = var.create_pvcs
