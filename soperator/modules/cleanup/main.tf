eb914a84ff378e1c5a82f3490ce645e4d157eb26
Uburro <grigorii.rochev@nebius.com>
2025-05-14 14:52:01 +0200

    fix clean up script

--- soperator/modules/cleanup/main.tf
+++ soperator/modules/cleanup/main.tf
@@ -1,9 +1,15 @@
+
 resource "terraform_data" "disk_cleanup" {
+    triggers_replace = {
+      parent_id = var.iam_project_id
+    }
+
   provisioner "local-exec" {
     when        = destroy
     interpreter = ["/bin/bash", "-c"]
+
     environment = {
-      "PARENT_ID": var.iam_project_id,
+      "PARENT_ID": self.triggers_replace.parent_id,
     }
     command     = "scripts/disk_cleanup.sh"
   }
