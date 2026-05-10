c1a8adf83ad74f9a38cbbf6f793ed77376afda97
David Tyler <davidtyler@microsoft.com>
2026-05-10 14:48:37 +0100

    Ensure that system node pool is up before other node pools
    This commit resolves an issue seen where a worker/nfs/accounting node pool is provisioned in the cluster before the system node pool. That causes kubernetes to attempt to place the cilium pods on the new node pool and fail (due to the taints) which in turn then blocks the remainder of the deployment.
    
    Explicit dependencies between pools may make deployment slightly slower but defends against this particular issue

--- soperator/modules/k8s/k8s_ng_controller.tf
+++ soperator/modules/k8s/k8s_ng_controller.tf
@@ -1,6 +1,7 @@
 resource "nebius_mk8s_v1_node_group" "controller" {
   depends_on = [
     nebius_mk8s_v1_cluster.this,
+    nebius_mk8s_v1_node_group.system.this,
     terraform_data.check_resource_preset_sufficiency,
   ]
 
