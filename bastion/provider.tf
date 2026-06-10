95a212f0c9ccf1e99d4f674874e731ac716adce1
Patrick Butler <pbutler@nebius.com>
2026-06-10 11:43:32 -0400

    fix(provider): updated all references to new public nebius providuer
    This changes all the old references to the nebius provider in our
    private repo to a new one in the public repos.  I left fixing version
    constraints to another patch where ever possible.

--- bastion/provider.tf
+++ bastion/provider.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     nebius = {
-      source  = "terraform-provider.storage.eu-north1.nebius.cloud/nebius/nebius"
-      version = ">= 0.5.174, < 0.6.0"
+      source  = "nebius/nebius"
+      version = ">= 0.6.0"
     }
   }
 }
