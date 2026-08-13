f9bcd69ffc4a7807db70c0bfb294c0275ed87e0f
Uburro <grigorii.rochev@nebius.com>
2026-08-13 14:56:20 +0200

    SCHED-1519: upgrade version fluxcd and set coorect settings

--- soperator/modules/fluxcd/variables.tf
+++ soperator/modules/fluxcd/variables.tf
@@ -6,5 +6,10 @@ variable "k8s_cluster_context" {
 variable "flux_version" {
   description = "The version of Flux to install."
   type        = string
-  default     = "v2.7.4"
+  default     = "v2.9.3"
+
+  validation {
+    condition     = can(regex("^v[0-9]+\\.[0-9]+\\.[0-9]+$", var.flux_version))
+    error_message = "flux_version must be a stable semantic version such as v2.9.3."
+  }
 }
