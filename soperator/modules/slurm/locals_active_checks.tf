9b7d7f925b086ba58d205e0c95f8de33851e3150
Barys Serankou <chessprofessor@nebius.com>
2026-09-07 13:44:55 +0200

    SCHED-2175: skip initial GPU checks in E2E

--- soperator/modules/slurm/locals_active_checks.tf
+++ soperator/modules/slurm/locals_active_checks.tf
@@ -17,7 +17,8 @@ locals {
     # Run what is relevant in E2E
     testing = {
       gpu-checks = {
-        suspend = true
+        suspend          = true
+        runAfterCreation = false
       }
       ssh-check = {
         k8sJobSpec = {
